b0b2852c67c2bb4edd91e53b8b650e8daa755811
Lawrence Mensah Ofoli <lawrenceofoli@gmail.com>
2023-08-05 13:39:53 +0000

    refactored salary history

--- frontend/src/views/SalaryHistory.js
+++ frontend/src/views/SalaryHistory.js
@@ -7,21 +7,31 @@ import {
   REACT_APP_LOAD,
   BASE_URL,
   URL_CREDENTIALS,
-  countryOptions,
+  countryOptions as countries,
 } from "../constants";
 
-const createDoughnutChartData = (data) => {
-  return {
-    labels: ["Permanent"],
-    datasets: [
-      {
-        data: [2],
-        backgroundColor: ["#FF6384"],
-      },
-    ],
-  };
+const URLS = {
+  live: {
+    category: (country) =>
+      `${BASE_URL}/${country}/categories?${URL_CREDENTIALS}`,
+    salaryHistory: (country, category) =>
+      `${BASE_URL}/${country}/history?${URL_CREDENTIALS}&category=${category}`,
+    searchData: (country) =>
+      `${BASE_URL}/${country}/search?${URL_CREDENTIALS}&results_per_page=30`,
+  },
+  dev: {
+    category: (c) => "/cached_responses/job_categories.json",
+    salaryHistory: (c, sc) => "/cached_responses/salary_history.json",
+    searchData: (c) => "/cached_responses/search_results.json",
+  },
 };
+const endpoints = REACT_APP_LOAD === "live" ? URLS.live : URLS.dev;
+const countryOptions = Object.keys(countries).map((value) => ({
+  key: countries[value],
+  value,
+}));
 
+//visualizations functions
 const createBarChartData = (data) => {
   // Your logic to extract data for Bar Chart
   console.log("createBarChartData: ", data);
@@ -36,191 +46,110 @@ const createBarChartData = (data) => {
     ],
   };
 };
-
+const createDoughnutChartData = (data) => {
+  return {
+    labels: ["Permanent"],
+    datasets: [
+      {
+        data: [2],
+        backgroundColor: ["#FF6384"],
+      },
+    ],
+  };
+};
 const SalaryHistory = () => {
-  const [salaryHistoryData, setJobData] = useState(null);
-  const [searchData, setSearchData] = useState(null);
+  const [country, selectCountry] = useState("");
+  const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
-  const [location, setLocation] = useState("us");
-  const [jobCategories, setJobCategories] = useState([]);
+  const [salaryHistory, setSalaryHistory] = useState({});
+  const [searchData, setSearchData] = useState(null);
   const [isSalaryHistoryLoading, setIsSalaryHistoryLoading] = useState(true);
   const [isSearchDataLoading, setIsSearchDataLoading] = useState(true);
 
-  const searchJobCategory = async (jobCategory) => {
+  const fetchSalaryHistory = async () => {
+    console.log("fetching salary history");
+
     setIsSalaryHistoryLoading(true);
-    setIsSearchDataLoading(true);
-    let endpoint = "/cached_responses/salary_history.json";
-
-    // http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id={YOUR API ID}&app_key={YOUR API KEY}&results_per_page=20&what=javascript%20developer&content-type=application/json
-    // select item with class jobCategory
-
-    if (REACT_APP_LOAD === "live") {
-      endpoint = `${BASE_URL}/${location}/history?${URL_CREDENTIALS}&category=${jobCategory}`;
-    }
-
-    console.log("fetching from endpoint: ", endpoint);
-    const responseHistory = await fetch(endpoint, {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .catch((error) => console.error(error));
-
-    setJobData(responseHistory);
-    setIsSalaryHistoryLoading(false);
-
-    endpoint = "/cached_responses/search_results.json";
-
-    if (REACT_APP_LOAD === "live") {
-      endpoint = `${BASE_URL}/${location}/search?${URL_CREDENTIALS}&results_per_page=30`;
-    }
-
-    const responseSearch = await fetch(endpoint, {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .catch((error) => console.error(error));
-
-    setSearchData(responseSearch);
+    const endpoint = endpoints.salaryHistory(country, selectedCategory);
+    const salHistResponse = await fetchData(endpoint);
+    if (salHistResponse.error !== null) setSalaryHistory(salHistResponse.data);
     setIsSearchDataLoading(false);
   };
 
-  const handleLocationChange = async (event) => {
-    setLocation(event.target.value);
-    // Populate categories supported for this country
-    let endpoint = "/cached_responses/job_categories.json";
-    if (REACT_APP_LOAD === "live") {
-      endpoint = `${BASE_URL}/${location}/categories?${URL_CREDENTIALS}`;
-    }
+  const fetchSearchData = async () => {
+    // http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id={YOUR API ID}&app_key={YOUR API KEY}&results_per_page=20&what=javascript%20developer&content-type=application/json
+    console.log("fetching job category");
 
-    const categories = await fetch(endpoint, {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .catch((error) => console.error(error));
-
-    setJobCategories(categories.results);
+    setIsSearchDataLoading(true);
+    const endpoint = endpoints.searchData(country);
+    const searchResponse = await fetchData(endpoint);
+    if (searchResponse.error !== null) setSearchData(searchResponse.data);
+    setIsSearchDataLoading(false);
   };
 
-  const handleCategoryChange = (event) => {
+  const onCountryChange = async (event) => {
+    const country = event.target.value;
+    selectCountry(country);
+    const endpoint = endpoints.category(country);
+    const { error, data } = await fetchData(endpoint);
+    if (error === null) setCategories(data.results);
+  };
+
+  const handleCategoryChange = (event) =>
     setSelectedCategory(event.target.value);
-    searchJobCategory(event.target.value);
-  };
 
-  const RadioGroup = ({ options, selectedOption, onChange }) => {
-    if (options.length === 0) {
-      // Return null if the options are not available yet
-      return null;
-    }
-    return (
-      <div className="flex items-center space-x-4">
-        <div className="max-h-64 overflow-y-auto">
-          {options.map((option) => (
-            <label
-              key={option.tag}
-              className="flex items-center cursor-pointer"
-            >
-              <input
-                name="jobCategory"
-                type="radio"
-                value={option.tag}
-                checked={selectedOption === option.tag}
-                onChange={(event) => onChange(event)}
-                className="form-radio h-4 w-4 text-indigo-600"
-              />
-              <span className="ml-2 text-gray-700">{option.label}</span>
-            </label>
-          ))}
-        </div>
-      </div>
-    );
+  const handleSearch = async () => {
+    await Promise.all([fetchSalaryHistory(), fetchSearchData()]);
   };
 
   return (
     <>
       <Navbar />
       <section
-        class="home-section section-hero overlay bg-image"
+        className="home-section section-hero overlay bg-image"
         id="home-section"
       >
-        <div class="container">
-          <div class="row align-items-center justify-content-center">
-            <div class="col-md-12">
-              <div class="mb-5 text-center">
-                <h1 class="text-white font-weight-bold">
+        <div className="container">
+          <div className="row align-items-center justify-content-center">
+            <div className="col-md-12">
+              <div className="mb-5 text-center">
+                <h1 className="text-white font-weight-bold">
                   Job Salary Visualization
                 </h1>
                 <p>
                   Have a look at the minimum, average and maximum salaries here!
                 </p>
               </div>
-              <form method="post" class="search-jobs-form">
-                <div class="row">
-                  <div class="col mb-5">
-                    <label for="underline_select" class="sr-only">
-                      Underline select
-                    </label>
-                    <select
-                      id="underline_select"
-                      name="location"
-                      class="text-gray-500 bg border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
-                      onChange={handleLocationChange}
-                    >
-                      {Object.entries(countryOptions).map(([key, value]) => (
-                        <option key={key} value={key}>
-                          {value}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
 
-                  <div class="col mb-5">
-                    <label for="underline_select" class="sr-only">
-                      Underline select
-                    </label>
-                    <select
-                      id="underline_select"
-                      name="location"
-                      class="text-gray-500 bg border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
-                      onChange={handleLocationChange}
-                    >
-                      {Object.entries(countryOptions).map(([key, value]) => (
-                        <option key={key} value={key}>
-                          {value}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                  <div class="col">
-                    <button className="mt-4 bg-[#F4ECC2] text-black h-12 hover:bg-[#e9dea9] hover:text-white rounded outline-none focus:outline-none">
-                      SUBMIT
-                    </button>
-                  </div>
+              <div className="row">
+                <SelectElement
+                  name="country"
+                  label="Country"
+                  // value={country}
+                  options={countryOptions}
+                  onChange={onCountryChange}
+                />
+                {categories.length !== 0 && (
+                  <SelectElement
+                    name="category"
+                    label="Category"
+                    options={categories.map((c) => ({
+                      key: c.label,
+                      value: c.tag,
+                    }))}
+                    onChange={handleCategoryChange}
+                  />
+                )}
+
+                <div className="col">
+                  <button
+                    className="mt-4 bg-[#F4ECC2] text-black h-12 hover:bg-[#e9dea9] hover:text-white rounded outline-none focus:outline-none"
+                    onClick={handleSearch}
+                  >
+                    SUBMIT
+                  </button>
                 </div>
-                <div class="row">
-                  <h1 className="text-2xl font-bold mb-4">
-                    Select a job category:
-                  </h1>
-                  {jobCategories.length > 0 && (
-                    <RadioGroup
-                      options={jobCategories}
-                      selectedOption={selectedCategory}
-                      onChange={handleCategoryChange}
-                    />
-                  )}
-                </div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
@@ -233,9 +162,7 @@ const SalaryHistory = () => {
               {isSalaryHistoryLoading ? (
                 <p>Loading salary history chart...</p>
               ) : (
-                salaryHistoryData && (
-                  <SalaryHistoryChart {...salaryHistoryData} />
-                )
+                <SalaryHistoryChart {...salaryHistory} />
               )}
             </div>
             <div className="basis-1/2">
@@ -299,3 +226,42 @@ const SalaryHistory = () => {
 };
 
 export default SalaryHistory;
+
+function SelectElement(props) {
+  const { id, name, label, options, onChange } = props;
+
+  return (
+    <div className="col mb-5">
+      <label className="sr-only">{label}</label>
+      <select
+        id={id}
+        name={name}
+        className="text-gray-500 bg border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+        onChange={onChange}
+      >
+        {options.map((option) => (
+          <option key={option.key} value={option.value}>
+            {option.key}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+const fetchData = async (url) => {
+  const FETCH_OPTIONS = {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const res = await fetch(url, FETCH_OPTIONS);
+    const data = await res.json();
+    return { error: null, data };
+  } catch (err) {
+    return { error: err.message, data: [] };
+  }
+};
